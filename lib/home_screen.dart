2dec3e97b051977e52bc8014ab68ffcd67bee1d7
azix-khan <azixkhan.55@gmail.com>
2024-09-03 23:38:13 +0500

    connecting hive db

---
+++ lib/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Hive"),
+      ),
+      body: const Column(
+        children: [],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
