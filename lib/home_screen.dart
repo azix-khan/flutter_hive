ad98219263d39a91a59b5efe8cba02ae2de9ca73
azix-khan <azixkhan.55@gmail.com>
2024-09-04 22:03:43 +0500

    making notes app with hive database

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -25,94 +25,95 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Flutter Hive Database"),
+        // title: const Text("Flutter Hive Database"),
+        title: const Text("Notes with Hive Database"),
         centerTitle: true,
       ),
       body: Column(
         children: [
-          FutureBuilder<Box>(
-            future: _boxFuture,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return Center(child: Text('Error: ${snapshot.error}'));
-              } else if (snapshot.hasData) {
-                // for box
-                final box = snapshot.data!;
-                return Column(
-                  children: [
-                    // for box 1
-                    Center(
-                      child: ListTile(
-                        title: Text(box.get('name').toString()),
-                        subtitle: Text(box.get('age').toString()),
-                        trailing: IconButton(
-                            onPressed: () {
-                              box.put('name', 'azix khan');
-                              box.put('age', 19);
-                              setState(() {});
-                            },
-                            icon: const Icon(Icons.edit)),
-                      ),
-                    ),
-                  ],
-                );
-              } else {
-                return const Center(child: Text('No data found'));
-              }
-            },
-          ),
-          // for box2
-          FutureBuilder<Box>(
-            future: _box2Future,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (snapshot.hasError) {
-                return Center(child: Text('Error: ${snapshot.error}'));
-              } else if (snapshot.hasData) {
-                final box2 = snapshot.data!;
-                return Column(
-                  children: [
-                    Center(
-                      child: ListTile(
-                        title: Text(box2.get('file').toString()),
-                        subtitle: Text(box2.get('data').toString()),
-                        trailing: IconButton(
-                            onPressed: () {
-                              box2.delete('file');
-                              box2.delete('data');
-                              setState(() {});
-                            },
-                            icon: const Icon(Icons.delete)),
-                      ),
-                    ),
-                  ],
-                );
-              } else {
-                return const Center(child: Text('No data found'));
-              }
-            },
-          ),
+          // FutureBuilder<Box>(
+          //   future: _boxFuture,
+          //   builder: (context, snapshot) {
+          //     if (snapshot.connectionState == ConnectionState.waiting) {
+          //       return const Center(child: CircularProgressIndicator());
+          //     } else if (snapshot.hasError) {
+          //       return Center(child: Text('Error: ${snapshot.error}'));
+          //     } else if (snapshot.hasData) {
+          //       // for box
+          //       final box = snapshot.data!;
+          //       return Column(
+          //         children: [
+          //           // for box 1
+          //           Center(
+          //             child: ListTile(
+          //               title: Text(box.get('name').toString()),
+          //               subtitle: Text(box.get('age').toString()),
+          //               trailing: IconButton(
+          //                   onPressed: () {
+          //                     box.put('name', 'azix khan');
+          //                     box.put('age', 19);
+          //                     setState(() {});
+          //                   },
+          //                   icon: const Icon(Icons.edit)),
+          //             ),
+          //           ),
+          //         ],
+          //       );
+          //     } else {
+          //       return const Center(child: Text('No data found'));
+          //     }
+          //   },
+          // ),
+          // // for box2
+          // FutureBuilder<Box>(
+          //   future: _box2Future,
+          //   builder: (context, snapshot) {
+          //     if (snapshot.connectionState == ConnectionState.waiting) {
+          //       return const Center(child: CircularProgressIndicator());
+          //     } else if (snapshot.hasError) {
+          //       return Center(child: Text('Error: ${snapshot.error}'));
+          //     } else if (snapshot.hasData) {
+          //       final box2 = snapshot.data!;
+          //       return Column(
+          //         children: [
+          //           Center(
+          //             child: ListTile(
+          //               title: Text(box2.get('file').toString()),
+          //               subtitle: Text(box2.get('data').toString()),
+          //               trailing: IconButton(
+          //                   onPressed: () {
+          //                     box2.delete('file');
+          //                     box2.delete('data');
+          //                     setState(() {});
+          //                   },
+          //                   icon: const Icon(Icons.delete)),
+          //             ),
+          //           ),
+          //         ],
+          //       );
+          //     } else {
+          //       return const Center(child: Text('No data found'));
+          //     }
+          //   },
+          // ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          var box = await Hive.openBox('database');
-          var box2 = await Hive.openBox('database2');
+          // var box = await Hive.openBox('database');
+          // var box2 = await Hive.openBox('database2');
 
-          box2.put('file', 'file1');
-          box2.put('data', 'file1 data');
+          // box2.put('file', 'file1');
+          // box2.put('data', 'file1 data');
 
-          box.put('name', 'azix');
-          box.put('age', 20);
-          box.put('details',
-              {'passion': 'software engineering', 'profession': 'developer'});
-          print(box.get('name'));
-          print(box.get('age'));
-          print(box.get('details'));
-          print(box.get('details')['passion']);
+          // box.put('name', 'azix');
+          // box.put('age', 20);
+          // box.put('details',
+          //     {'passion': 'software engineering', 'profession': 'developer'});
+          // print(box.get('name'));
+          // print(box.get('age'));
+          // print(box.get('details'));
+          // print(box.get('details')['passion']);
         },
         child: const Icon(Icons.add),
       ),
