055e44c626910c43d4355e5864fa0f22e83f69c5
azix-khan <azixkhan.55@gmail.com>
2024-09-04 22:06:14 +0500

    removed unused code

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,7 +1,4 @@
-// ignore_for_file: avoid_print
-
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,16 +8,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  Future<Box>? _boxFuture;
-  Future<Box>? _box2Future;
-
-  @override
-  void initState() {
-    super.initState();
-    _boxFuture = Hive.openBox('database');
-    _box2Future = Hive.openBox('database2');
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
