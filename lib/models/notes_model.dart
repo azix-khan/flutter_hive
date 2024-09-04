5615e5d37c25e1907467ae2a42273f866a9c439a
azix-khan <azixkhan.55@gmail.com>
2024-09-04 22:23:13 +0500

    command added

---
+++ lib/models/notes_model.dart
@@ -0,0 +1,23 @@
+import 'package:hive/hive.dart';
+part 'notes_model.g.dart';
+
+@HiveType(typeId: 0)
+class NotesModel {
+  //give hive unique field
+  @HiveField(0)
+  String title;
+
+  // give hive unique field
+  @HiveField(1)
+  String description;
+
+  /*
+  -> after this run this command
+  -> flutter packages pub run build_runner build 
+  */
+
+  NotesModel({
+    required this.title,
+    required this.description,
+  });
+}
