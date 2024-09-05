5f2d16c7335dd70df6290cefae71675a33e4e42d
azix-khan <azixkhan.55@gmail.com>
2024-09-05 23:05:32 +0500

    show the db data on screen through valueListenableBuilder

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hive/boxes/boxes.dart';
 import 'package:flutter_hive/models/notes_model.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -20,8 +20,31 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text("Notes with Hive Database"),
         centerTitle: true,
       ),
-      body: const Column(
-        children: [],
+      body: ValueListenableBuilder<Box<NotesModel>>(
+        valueListenable: Boxes.getData().listenable(),
+        builder: (context, box, _) {
+          // remeber to convert Box data into list form
+          var data = box.values.toList().cast<NotesModel>();
+          return ListView.builder(
+            itemCount: box.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 12, horizontal: 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(data[index].title.toString()),
+                      Text(data[index].description.toString()),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
@@ -55,10 +78,10 @@ class _HomeScreenState extends State<HomeScreen> {
                   final box = Boxes.getData();
                   box.add(data);
                   // now save the data
-                  data.save();
-                  if (kDebugMode) {
-                    print(box);
-                  }
+                  // data.save(); -> no need after adding .listnable()
+                  // if (kDebugMode) {
+                  //   print(box);
+                  // }
                   // now clear the field
                   titleController.clear();
                   descriptionController.clear();
